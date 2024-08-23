1aed15b833af40dbd6e7926eff356d331e7b7db5
Matt Dyor <matt@dyor.com>
2024-08-23 16:16:57 -0700

    Initial Commit

---
+++ bumps-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bumps-ios
+//
+//  Created by Matt Dyor on 8/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bumps-ios/bumps_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bumps_iosApp.swift
+//  bumps-ios
+//
+//  Created by Matt Dyor on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct bumps_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
